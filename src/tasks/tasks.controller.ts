75cc202c5daec3bb7f281cef21b7758b5f8f5f91
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-21 11:36:33 +0200

    🚧 tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,7 +16,34 @@ export class TaskController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(@Query() query: any) {
+    console.log(query);
+
     return this.tasksService.getTasks();
+  }
+
+  @Get('/:id')
+  getTask(@Param('id') id: string) {
+    return this.tasksService.getTask(parseInt(id));
+  }
+
+  @Post()
+  createTask(@Body() task: any) {
+    return this.tasksService.createTask(task);
+  }
+
+  @Put()
+  updateTask() {
+    return this.tasksService.updateTask();
+  }
+
+  @Patch()
+  updateTaskStatus() {
+    return this.tasksService.updateTaskStatus();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class TasksService {
+  private tasks = [];
+
   getTasks() {
-    return ['Task 1', 'Task 2', 'Task 3'];
+    return this.tasks;
+  }
+
+  getTask(id: number) {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      return new NotFoundException();
+    }
+
+    return task;
+  }
+
+  createTask(task: any) {
+    this.tasks.push({
+      ...task,
+      title: `Task ${this.tasks.length + 1}`,
+      id: this.tasks.length + 1,
+    });
+
+    return task;
+  }
+
+  updateTask() {
+    return 'updating task';
+  }
+
+  updateTaskStatus() {
+    return 'updating status';
+  }
+
+  deleteTask() {
+    return 'delete task';
   }
 }
